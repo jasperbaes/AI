c3e13f7300f0fde7432ed9f11c2a36d146626f09
jasper <jasper.baes@gmail.com>
2019-11-19 13:47:05 +0100

    add avg diff of prediction

--- AI_Celcius2Kelvin.py
+++ AI_Celcius2Kelvin.py
@@ -9,7 +9,7 @@ celcius_q = np.array([-200, -90, -40,  14, 32, 46, 59, 72, 100],  dtype=float)
 kelvin_a    = np.array([73.15, 183.15, 233.15, 287.15,  305.15,  319.15, 332.15, 345.15, 373.15],  dtype=float)
 celcius_testset = [-50, 0, 50, 100, 150]
 
-print("Calculating {} predictions by {} inputs... ".format(len(celcius_testset), len(celcius_q)))
+print("Training dataset...")
 
 l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
 model = tf.keras.Sequential([l0])
@@ -17,6 +17,13 @@ model = tf.keras.Sequential([l0])
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
 history = model.fit(celcius_q, kelvin_a, epochs=9000, verbose=False)
 
+diff = 0.0
+print("Calculating {} predictions by {} inputs... ".format(len(celcius_testset), len(celcius_q)))
+
 for cel in celcius_testset:
-    print("[{}] ({}, {})".format(colored('+', 'green'), cel, model.predict([cel])[0][0]))
+    answer = model.predict([cel])[0][0]
+    print("[{}] ({}, {})".format(colored('+', 'green'), cel, answer))
+    diff += abs((cel+273.15)-answer)
+
 print("Layer weights: {} and {}".format(l0.get_weights()[0][0][0], l0.get_weights()[1][0]))
+print("Avg difference: {}".format(diff/len(celcius_testset)))
