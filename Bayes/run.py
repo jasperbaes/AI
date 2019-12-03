6943d2cafb50e78ae272835580a46806baa65e3b
jasper <jasper.baes@gmail.com>
2019-12-03 23:36:02 +0100

    small changes

--- Bayes/run.py
+++ Bayes/run.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python 
+import train_data as data
 import math
 
-# Training data
-o1 = [1,0,0,1,0,0,1,0,1,1,0,0,1,0,0,0,0,0,1,0]
-o2 = [0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,1]
-o3 = [0,0,1,1,0.5,0,0,0,0,1,0.5,0.5,0,1,0.5,0,0,0,1,0]
-o4 = [1,0,0,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,0,1]
-res = [1,0,0,1,0,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0]
-
-# What we know
-r1 = 1
-r2 = 0
-r3 = 1
-r4 = 0
-
 def checkInput():
-    return len(o1) == len(o2) == len(o3) == len(o4)
+    return len(data.o1) == len(data.o2) == len(data.o3) == len(data.o4)
 
-def calc_change(val_true, len, o1, o2, o3, o4):
+def calc_chance(val_true, len, o1, o2, o3, o4):
     return (val_true/len) * (o1/val_true) * (o2/val_true) * (o3/val_true) * (o4/val_true)
 
 def stats_res():
     count = 0
-    for val in res:
+    for val in data.res:
         if (val == 1):
             count += 1
     return count
@@ -30,7 +18,7 @@ def stats_res():
 def calc_amount(array, r, type):
     count = 0
     for index, val in enumerate(array):
-        if (res[index] == type and val == r):
+        if (data.res[index] == type and val == r):
             count += 1
     return count
 
@@ -40,26 +28,23 @@ if (not checkInput()):
 else:
     print("Calculating...")
 
-    res1 = calc_change(
-        calc_amount(res, 1, 1),
-        len(res),
-        calc_amount(o1, r1, 1),
-        calc_amount(o2, r2, 1),
-        calc_amount(o3, r3, 1),
-        calc_amount(o4, r4, 1)
+    data.res1 = calc_chance(
+        calc_amount(data.res, 1, 1),
+        len(data.res),
+        calc_amount(data.o1, data.r1, 1),
+        calc_amount(data.o2, data.r2, 1),
+        calc_amount(data.o3, data.r3, 1),
+        calc_amount(data.o4, data.r4, 1)
     )
 
-    res2 = calc_change(
-        calc_amount(res, 0, 0),
-        len(res),
-        calc_amount(o1, r1, 0),
-        calc_amount(o2, r2, 0),
-        calc_amount(o3, r3, 0),
-        calc_amount(o4, r4, 0)
+    data.res2 = calc_chance(
+        calc_amount(data.res, 0, 0),
+        len(data.res),
+        calc_amount(data.o1, data.r1, 0),
+        calc_amount(data.o2, data.r2, 0),
+        calc_amount(data.o3, data.r3, 0),
+        calc_amount(data.o4, data.r4, 0)
     )
 
-    print(round(res1, 4), round(res2, 4))
-
-    print("The result is {0}".format(res1 > res2))
-
-
+    print("{0} vs {1}".format(round(data.res1, 4), round(data.res2, 4)))
+    print("The chance of {0} is {1} times bigger then {2} ".format(data.res1 > data.res2, round(data.res1/data.res2 if data.res1 > data.res2 else data.res2/data.res1, 2) , data.res1 < data.res2))
