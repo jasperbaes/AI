7dd394ccca5dfa5bd2ff00ff91f979a45bae1f24
jasper <jasper.baes@gmail.com>
2019-12-04 10:59:46 +0100

    add dynamic input

--- Bayes/run.py
+++ Bayes/run.py
@@ -2,24 +2,24 @@
 import train_data as data
 import math
 
+LEN_INPUT = len(data.o)
+
 def checkInput():
-    return len(data.o1) == len(data.o2) == len(data.o3) == len(data.o4)
+    len_input = len(data.o[0])
+    for arr in data.o:
+        if (len(arr) != len(data.o[0])):
+            return False
+    return True
 
 def calc_chance(val_true, len, o1, o2, o3, o4):
     return (val_true/len) * (o1/val_true) * (o2/val_true) * (o3/val_true) * (o4/val_true)
 
-def stats_res():
-    count = 0
-    for val in data.res:
-        if (val == 1):
-            count += 1
-    return count
-
 def calc_amount(array, r, type):
     count = 0
     for index, val in enumerate(array):
-        if (data.res[index] == type and val == r):
+        if (data.o[LEN_INPUT-1][index] == type and val == r):
             count += 1
+    print(count)
     return count
 
 
@@ -29,21 +29,21 @@ else:
     print("Calculating...")
 
     data.res1 = calc_chance(
-        calc_amount(data.res, 1, 1),
-        len(data.res),
-        calc_amount(data.o1, data.r1, 1),
-        calc_amount(data.o2, data.r2, 1),
-        calc_amount(data.o3, data.r3, 1),
-        calc_amount(data.o4, data.r4, 1)
+        calc_amount(data.o[LEN_INPUT-1], 1, 1),
+        len(data.o[LEN_INPUT-1]),
+        calc_amount(data.o[0], data.r[0], 1),
+        calc_amount(data.o[1], data.r[1], 1),
+        calc_amount(data.o[2], data.r[2], 1),
+        calc_amount(data.o[3], data.r[3], 1)
     )
 
     data.res2 = calc_chance(
-        calc_amount(data.res, 0, 0),
-        len(data.res),
-        calc_amount(data.o1, data.r1, 0),
-        calc_amount(data.o2, data.r2, 0),
-        calc_amount(data.o3, data.r3, 0),
-        calc_amount(data.o4, data.r4, 0)
+        calc_amount(data.o[LEN_INPUT-1], 0, 0),
+        len(data.o[LEN_INPUT-1]),
+        calc_amount(data.o[0], data.r[0], 0),
+        calc_amount(data.o[1], data.r[1], 0),
+        calc_amount(data.o[2], data.r[2], 0),
+        calc_amount(data.o[3], data.r[3], 0)
     )
 
     print("{0} vs {1}".format(round(data.res1, 4), round(data.res2, 4)))
